adfd7c751fcf134b78dcec0c50de9d1ecc051f0a
Piyush Raj <81475553+piyushraj0031@users.noreply.github.com>
2023-12-07 12:25:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///piyushtodo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(200),nullable=False)
+    desc=db.Column(db.String(500),nullable=False)
+    date_created=db.Column(db.DateTime,default=datetime.utcnow)
+    
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+@app.route('/', methods=['GET','POST'])
+def hello_world():
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        piyushtodo=Todo(title=title, desc=desc)
+        db.session.add(piyushtodo)
+        db.session.commit()
+    allTodo=Todo.query.all()
+    return render_template('index.html',allTodo=allTodo)
+
+@app.route('/show')
+def products():
+    allTodo=Todo.query.all()
+    print(allTodo)
+    return 'This is the Product'
+
+@app.route('/update/<int:sno>',methods=['GET','POST'])
+def update(sno):
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        piyushtodo=Todo.query.filter_by(sno=sno).first()
+        piyushtodo.title=title
+        piyushtodo.desc=desc
+        db.session.add(piyushtodo)
+        db.session.commit()
+        return redirect("/")
+    todo=Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html',todo=todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo=Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=8000)
